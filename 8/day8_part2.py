5dc6af8b905d7917264fbd04d8695368a4a09f36
Jonathan Hook <jonathan.hook@york.ac.uk>
2023-12-23 17:23:02 +0000

    Day 8 part 2 complete

--- 8/day8_part2.py
+++ 8/day8_part2.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 class Node:
@@ -17,27 +18,34 @@ def parseInput(input):
     data.append(lines[0].strip())
 
     nodes = list()
-    for l in lines:
+    for l in lines[2:]:
         nodes.append(Node(l[0:3], l[7:10], l[12:15]))
 
     data.append(nodes)
 
     return data
 
-def fineNode(val, map):
+def findAllEnding(e, map):
+    all = list()
+    for m in map:
+        if m.val[2:3] == e:
+            all.append(m)
+    return all
+
+def findNode(val, map):
     for m in map:
         if m.val == val:
             return m
     return None
 
-def followDirections(start, directions, map):
+def followPath(start, directions, map):
     steps = 0
     pos = start
     while True:
         for d in directions:
-            node = fineNode(pos, map)            
+            node = findNode(pos, map)            
             
-            if(node.val == 'ZZZ'):
+            if(node.val[2:3] == 'Z'):
                 return steps
             
             steps += 1
@@ -46,10 +54,16 @@ def followDirections(start, directions, map):
             elif d == 'R':
                 pos = node.right
 
+def findLCMs(directions, map):
+    multiples = list()
+    for t in findAllEnding('A', map):
+        multiples.append(followPath(t.val, directions, map))
+
+    return multiples
+
 def main(input):
     data = parseInput(input)
+    multiples = findLCMs(data[0], data[1])
+    print(math.lcm(*multiples))
 
-    steps = followDirections('11A', data[0], data[1])
-    print(steps)
-
-main('Input.txt')
+main('input.txt')
